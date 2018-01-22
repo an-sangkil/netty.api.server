f58bebc72177e998f76650c3b4e44eadf26f3534
An SangKil <sangkil.an@cj.net>
2018-01-22 21:28:44 +0900

    Echo Server 인바운드값 테스트

--- src/main/java/cj/enm/mezzo/EhcoServerHandler.java
+++ src/main/java/cj/enm/mezzo/EhcoServerHandler.java
@@ -1,15 +1,20 @@
 package cj.enm.mezzo;
 
-import io.netty.channel.ChannelHandlerAdapter;
-import io.netty.channel.ChannelHandlerContext;
+import java.nio.charset.Charset;
 
-public class EhcoServerHandler extends ChannelHandlerAdapter {
-	// 채널을 읽을 때 동작할 코드를 정의 합니다.
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EhcoServerHandler extends ChannelInboundHandlerAdapter {
+	// 채널을 읽을 때 동작할 코드를 정의 합니다
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg)
 			throws Exception {
+		
+		String readMessage = ((ByteBuf)msg).toString(Charset.defaultCharset());
 		ctx.write(msg); // 메시지를 그대로 다시 write 합니다.
-		System.out.println(msg);
+		System.out.println(readMessage);
 	}
 
 	// 채널 읽는 것을 완료했을 때 동작할 코드를 정의 합니다.
