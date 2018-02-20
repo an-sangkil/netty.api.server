d8b259ce587ff5c9640888561eefb6a9f20e3a39
An SangKil <sangkil.an@cj.net>
2018-02-20 09:44:14 +0900

    채널 초기화 소켓 설정

---
+++ src/main/java/com/mezzomedia/config/ApplicationInitializerConfig.java
@@ -0,0 +1,32 @@
+package com.mezzomedia.config;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * 
+ * <pre>
+ * Class Name  : ApplicationConfig.java
+ * Description : 
+ * Modification Information
+ *
+ *    수정일　　　 　　  		수정자　　　     			  수정내용
+ *    ────────────   ─────────   ───────────────────────────────
+ *    2018. 2. 19.          skan               최초생성
+ * </pre>
+ *
+ * @author skan
+ * @since 2018. 2. 19.
+ * @version 
+ *
+ * Copyright (C) 2018 by Mezzomedia.Inc. All right reserved.
+ */
+public class ApplicationInitializerConfig extends ChannelInitializer<SocketChannel> {
+
+	@Override
+	protected void initChannel(SocketChannel arg0) throws Exception {
+		
+		
+	}
+
+}
