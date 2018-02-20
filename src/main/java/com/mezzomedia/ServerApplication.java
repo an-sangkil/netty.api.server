24d0a889f17839304bce645b32b2621b8547ef43
An SangKil <sangkil.an@cj.net>
2018-02-20 09:41:48 +0900

    Spring env 설정 추가,

--- src/main/java/com/mezzomedia/ServerApplication.java
+++ src/main/java/com/mezzomedia/ServerApplication.java
@@ -1,11 +1,33 @@
 package com.mezzomedia;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.util.EnvironmentTestUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 
+/**
+ * 
+ * <pre>
+ * Class Name  : ServerApplication.java
+ * Description : AD서버 스타트 파일  SpringBoot configuration  
+ * Modification Information
+ *
+ *    수정일　　　 　　  		수정자　　　     			  수정내용
+ *    ────────────   ─────────   ───────────────────────────────
+ *    2018. 2. 19.          skan               최초생성
+ * </pre>
+ *
+ * @author skan
+ * @since 2018. 2. 19.
+ * @version 
+ *
+ * Copyright (C) 2018 by Mezzomedia.Inc. All right reserved.
+ */
 @SpringBootApplication
 public class ServerApplication {
 	
@@ -13,7 +35,16 @@ public class ServerApplication {
 	private ApplicationContext context;
 	
 	public static void main (String[] args) throws Exception {
-		ConfigurableApplicationContext context = SpringApplication.run(ServerApplication.class, args);
+		
+		// SpringBoot run 
+		SpringApplication application = new SpringApplication(ServerApplication.class);
+		
+		
+		ConfigurableEnvironment environment = new StandardEnvironment();
+		environment.setDefaultProfiles("dev");
+		application.setEnvironment(environment);
+		//application.printBanner();
+		ConfigurableApplicationContext context = application.run(args);
 		MezzoAdServer mezzoAdServer = context.getBean(MezzoAdServer.class);
 		mezzoAdServer.start();
 	}
