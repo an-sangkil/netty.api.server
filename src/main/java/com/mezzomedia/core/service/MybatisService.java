2a8e63395beedde965775fd84d5d0088777aee7e
An SangKil <mycup@nate.com>
2018-04-18 17:35:23 +0900

    mysql connector 추가

---
+++ src/main/java/com/mezzomedia/core/service/MybatisService.java
@@ -0,0 +1,13 @@
+package com.mezzomedia.core.service;
+
+/**
+ * <pre>
+ * Description :
+ * @author mezzomedia
+ * @since 2018.04.18
+ * @version
+ *
+ * Copyright (C) 2018 by Mezzomedia.Inc. All right reserved.
+ */
+public class MybatisService {
+}
