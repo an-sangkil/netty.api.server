3ee6f5fadebc2275997524aa6074610f9c33bc0f
An SangKil <mycup@nate.com>
2018-05-15 16:48:03 +0900

    로그파일 남기기 위한 유틸 추가중

---
+++ src/test/java/com/mezzomedia/core/file/FileChannelWriteTest.java
@@ -0,0 +1,16 @@
+package com.mezzomedia.core.file;
+
+import org.junit.Test;
+
+public class FileChannelWriteTest {
+	
+	@Test
+	public void fileWrite() {
+		
+		FileChannelWrite fileChannelWrite = new FileChannelWrite();
+		for (int i = 1 ; i <= 10 ; i ++ ) {
+			fileChannelWrite.fileWrite("d:/test", "test1.txt" , "hello"+i);	
+		}
+	}
+
+}
