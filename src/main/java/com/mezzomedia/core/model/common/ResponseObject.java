47a9eb8bbabd6bd36e18bb35ff46435c709d8b51
An SangKil <mycup@nate.com>
2018-05-29 16:17:00 +0900

    공통 Response 코드 적용중

---
+++ src/main/java/com/mezzomedia/core/model/common/ResponseObject.java
@@ -0,0 +1,47 @@
+package com.mezzomedia.core.model.common;
+
+import com.mezzomedia.core.code.CommonCode;
+
+public abstract class ResponseObject<T> {
+	
+	/** 응답시 넘겨줄 객체 를 제네릭으로 선언하여 사용한다. */
+	private T object;
+	
+	/** 응답 메세지 */
+	private String responseMessage;
+	
+	/**
+	 * 상태코드
+	 * {
+	 * 		SUCCESS : 성공
+	 * 		FAIL	: 실패
+	 * } 
+	 * 
+	 */
+	private CommonCode stateCode;
+
+	public T getObject() {
+		return object;
+	}
+
+	public void setObject(T object) {
+		this.object = object;
+	}
+
+	public String getResponseMessage() {
+		return responseMessage;
+	}
+
+	public void setResponseMessage(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}
+
+	public CommonCode getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(CommonCode stateCode) {
+		this.stateCode = stateCode;
+	}
+
+}
