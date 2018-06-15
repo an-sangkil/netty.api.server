e2d273d28aee2e2240d6b7f2b6e781f41008e8d8
An SangKil <mycup@nate.com>
2018-06-15 19:16:15 +0900

    커멘드러너 적용, 사용중인 빈 종류

--- src/main/java/com/mezzomedia/server/SpringAdverApplication.java
+++ src/main/java/com/mezzomedia/server/SpringAdverApplication.java
@@ -4,14 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.test.util.EnvironmentTestUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
+
+import java.util.Arrays;
 
 /**
  * 
@@ -40,8 +44,7 @@ public class SpringAdverApplication {
 	private ApplicationContext context;
 	
 	public static void main (String[] args) throws Exception {
-		
-		
+
 		//AnnotationConfigApplicationContext;
 		// SpringBoot run 
 		SpringApplication application = new SpringApplication(SpringAdverApplication.class);
@@ -59,6 +62,22 @@ public class SpringAdverApplication {
 		advertisementServer.start();
 		
 	}
-	
-	
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+
+
 }
