6dd6492a4499537fecac3aa8c057bf764816997c
An SangKil <mycup@nate.com>
2018-07-11 15:24:31 +0900

    중요내역 코멘트

--- src/main/java/com/mezzomedia/server/AdvertisementServer.java
+++ src/main/java/com/mezzomedia/server/AdvertisementServer.java
@@ -16,6 +16,9 @@ import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * <pre>
@@ -45,25 +48,45 @@ public class AdvertisementServer {
 			parentGroup = new NioEventLoopGroup();
 			childGroup = new NioEventLoopGroup();
 		}
-		
+
 		try {
 			ServerBootstrap sb = new ServerBootstrap();
 			sb.group(parentGroup, childGroup)
 					.channel(Epoll.isAvailable() ? EpollServerSocketChannel.class : NioServerSocketChannel.class)
+
+					// 외부 요청을 받는 채널 옵션
 					.option(ChannelOption.SO_BACKLOG, 300)
+
+					// 커넥션이 맺어진 후 생성되는 자식 채널을 위한 옵션
+					//   - keepAlive option 지정
+					//.childOption()
+
 					// .handler(new LoggingHandler(LogLevel.INFO))
-					// 새롭게 액세스된 Channel을 처리합니다. 
-					// ChannelInitializer는 특별한 핸들러로 새로운 Channel의 환경 구성을 도와 주는 것이 목적입니다.
+
+					// 새롭게 액세스된 Channel을 처리합니다. ChannelInitializer는 특별한 핸들러로 새로운 Channel의 환경 구성을 도와 주는 것이 목적입니다.
+					//  - 연결이 수락될때마다 호출
+
 					.childHandler(new ApplicationChannelInitializer());
+
 
 			// 인커밍 커넥션을 액세스하기 위해 바인드하고 시작합니다.
 			ChannelFuture cf = sb.bind(tcpPort).sync();
 
 			// 서버 소켓이 닫힐때까지 대기합니다.
 			cf.channel().closeFuture().sync();
+
+
+//			List<ChannelFuture> futures = new ArrayList<>();
+//			futures.add(sb.bind(8080));
+//			futures.add(sb.bind(8081));
+//			for (ChannelFuture channelFuture : futures){
+//				channelFuture.sync();
+//			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
+			// 대기중이 이벤트와 작업을 모두 처리 하거나 종료.
 			parentGroup.shutdownGracefully();
 			childGroup.shutdownGracefully();
 		}
